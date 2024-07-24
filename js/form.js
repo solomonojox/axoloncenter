361f9fcef8d5d7b6da63feb59d69dd1b039a1387
Akpa Solomon <solomonakpas@gmail.com>
2024-07-24 20:15:07 +0100

    first commit

--- js/form.js
+++ js/form.js
@@ -14,85 +14,88 @@ const customerInfo = {
 }
 
 const submitBtn = document.querySelector('.submitBtn');
-submitBtn.addEventListener('click', function validateMyForm() {
-  let isValid = true;
 
-  // Clear previous errors
-  document.querySelectorAll('.error').forEach(error => error.textContent = '');
+submitBtn.addEventListener('click', function validateMyForm(e) {
+    e.preventDefault()
+    let isValid = true;
 
-  // Get form fields
-  const firstName = document.getElementById('firstName').value.trim();
-  const lastName = document.getElementById('lastName').value.trim();
-  const email = document.getElementById('email').value.trim();
-  const phone = document.getElementById('phone').value.trim();
-  const address = document.getElementById('address').value.trim();
-  const eventType = document.getElementById('eventType').value;
-  const guestSize = document.getElementById('guestSize').value;
-  const eventDate = document.getElementById('eventDate').value;
-  const eventTime = document.getElementById('eventTime').value;
+    // Clear previous errors
+    document.querySelectorAll('.error').forEach(error => error.textContent = '');
 
-  customerInfo.first_name += firstName
-  customerInfo.last_name += lastName
-  customerInfo.email += email
-  customerInfo.phone += phone
-  customerInfo.address += address
-  customerInfo.event_type += eventType
-  customerInfo.guest_size += guestSize
-  customerInfo.event_date += eventDate
-  customerInfo.event_time += eventTime
+    // Get form fields
+    const firstName = document.getElementById('firstName').value.trim();
+    const lastName = document.getElementById('lastName').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const phone = document.getElementById('phone').value.trim();
+    const address = document.getElementById('address').value.trim();
+    const eventType = document.getElementById('eventType').value;
+    const guestSize = document.getElementById('guestSize').value;
+    const eventDate = document.getElementById('eventDate').value;
+    const eventTime = document.getElementById('eventTime').value;
 
-  // console.log(customerInfo)
+    customerInfo.first_name += firstName
+    customerInfo.last_name += lastName
+    customerInfo.email += email
+    customerInfo.phone += phone
+    customerInfo.address += address
+    customerInfo.event_type += eventType
+    customerInfo.guest_size += guestSize
+    customerInfo.event_date += eventDate
+    customerInfo.event_time += eventTime
 
-  // Validate each field
-  if (firstName === '') {
-      document.getElementById('firstNameError').textContent = 'First name is required';
-      isValid = false;
-  }
-  if (lastName === '') {
-      document.getElementById('lastNameError').textContent = 'Last name is required';
-      isValid = false;
-  }
-  if (email === '') {
-      document.getElementById('emailError').textContent = 'Email is required';
-      isValid = false;
-  }
-  if (phone === '') {
-      document.getElementById('phoneError').textContent = 'Phone number is required';
-      isValid = false;
-  }
-  if (address === '') {
-      document.getElementById('addressError').textContent = 'House address is required';
-      isValid = false;
-  }
-  if (eventType === '') {
-      document.getElementById('eventTypeError').textContent = 'required';
-      isValid = false;
-  }
-  if (guestSize === '') {
-      document.getElementById('guestSizeError').textContent = 'required';
-      isValid = false;
-  }
-  if (eventDate === '') {
-      document.getElementById('eventDateError').textContent = 'required';
-      isValid = false;
-  }
-  if (eventTime === '') {
-      document.getElementById('eventTimeError').textContent = 'required';
-      isValid = false;
-  }
+    // console.log(customerInfo)
 
-  if(isValid){
+    // Validate each field
+    if (firstName === '') {
+        document.getElementById('firstNameError').textContent = 'First name is required';
+        isValid = false;
+    }
+    if (lastName === '') {
+        document.getElementById('lastNameError').textContent = 'Last name is required';
+        isValid = false;
+    }
+    if (email === '') {
+        document.getElementById('emailError').textContent = 'Email is required';
+        isValid = false;
+    }
+    if (phone === '') {
+        document.getElementById('phoneError').textContent = 'Phone number is required';
+        isValid = false;
+    }
+    if (address === '') {
+        document.getElementById('addressError').textContent = 'House address is required';
+        isValid = false;
+    }
+    if (eventType === '') {
+        document.getElementById('eventTypeError').textContent = 'required';
+        isValid = false;
+    }
+    if (guestSize === '') {
+        document.getElementById('guestSizeError').textContent = 'required';
+        isValid = false;
+    }
+    if (eventDate === '') {
+        document.getElementById('eventDateError').textContent = 'required';
+        isValid = false;
+    }
+    if (eventTime === '') {
+        document.getElementById('eventTimeError').textContent = 'required';
+        isValid = false;
+    }
+
+    if(isValid){
     customerDetail.push(customerInfo)
     alert('Thank you for your booking. We will contact you shortly.')
     location.reload();
-  }
-  
-  localStorage.setItem('info', JSON.stringify(customerDetail))
-  return isValid;
+    }
+
+    localStorage.setItem('info', JSON.stringify(customerDetail))
+    return isValid;
 })
 
 const clearBtn = document.getElementById('clearBtn');
 clearBtn.addEventListener('click', function() {
-  localStorage.clear();
-  location.reload();
+    // e.preventDefault();
+    localStorage.clear();
+    location.reload();
 })
---
+++ js/sendtoMail.js
@@ -0,0 +1,15 @@
+function sendEmail() {
+  Email.send({
+    SecureToken : "f4c34865-b185-4e7d-a984-a5edc6cfbd08",
+    // Port: 2525, Server
+    // Host : "smtp.elasticemail.com",
+    // Username : 'solomonakpas@gmail.com',
+    // Password : "869A83FE9CA3213A270423465ACA234DC983",
+    To : 'solomonakpas@gmail.com',
+    From : "solomonakpas@gmail.com",
+    Subject : "New Contact Form Enquiry",
+    Body : "Name: " + document.getElementById("fullName").value + "<br> Email: " + document.getElementById("email").value + "<br> Phone: " + document.getElementById("phone").value + "<br> Address: " + document.getElementById("address").value + "<br> Event Type: " + document.getElementById("eventType").value + "<br> Guest size: " + document.getElementById("guestSize").value + "<br> Event Date: " + document.getElementById("eventDate").value + "<br> Event Time: " + document.getElementById("eventTime").value
+  }).then(
+    message => alert('Your message has been sent!')
+  );
+}
