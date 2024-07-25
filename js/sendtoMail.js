fd08970549882b16537a8991c56c609729a31aee
Akpa Solomon <solomonakpas@gmail.com>
2024-07-25 19:38:09 +0100

    first commit

--- js/sendtoMail.js
+++ js/sendtoMail.js
@@ -40,8 +40,6 @@ submitBtn.addEventListener('click', function validateMyForm(e) {
     customerInfo.event_date += eventDate
     customerInfo.event_time += eventTime
 
-    // console.log(customerInfo)
-
     // Validate each field
     if (fullName === '') {
         document.getElementById('fullNameError').textContent = 'First name is required';
@@ -77,15 +75,30 @@ submitBtn.addEventListener('click', function validateMyForm(e) {
     }
 
     if(isValid){
-      sendEmail()
-      customerDetail.push(customerInfo)
+      sendEmail();
+      customerDetail.push(customerInfo);
+      openPopup();
+      clearForm();
     }
-
+    
     localStorage.setItem('info', JSON.stringify(customerDetail))
     return isValid;
-})
-function sendEmail() {
+  })
+
+  // Clear form fields
+  function clearForm() {
+    document.getElementById('fullName').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('phone').value = '';
+    document.getElementById('address').value = '';
+    document.getElementById('eventType').value = '';
+    document.getElementById('guestSize').value = '';
+    document.getElementById('eventDate').value = '';
+    document.getElementById('eventTime').value = '';
+  }
+  function sendEmail() {
   
+  // Send Email
   Email.send({
     SecureToken : "f4c34865-b185-4e7d-a984-a5edc6cfbd08",
     // Port: 2525, Server
@@ -96,7 +109,23 @@ function sendEmail() {
     From : "solomonakpas@gmail.com",
     Subject : "New Contact Form Enquiry",
     Body : "Name: " + document.getElementById("fullName").value + "<br> Email: " + document.getElementById("email").value + "<br> Phone: " + document.getElementById("phone").value + "<br> Address: " + document.getElementById("address").value + "<br> Event Type: " + document.getElementById("eventType").value + "<br> Guest size: " + document.getElementById("guestSize").value + "<br> Event Date: " + document.getElementById("eventDate").value + "<br> Event Time: " + document.getElementById("eventTime").value
-  }).then(
-    message => alert('Your message has been sent')
-  );
-}
+    })
+    // .then(
+    //   message => alert('Your message has been sent')
+    // );
+  }
+
+  // Open Pop up
+  let popup = document.querySelector(".popup-content")
+  let open = document.getElementById("open-popup")
+  open.addEventListener("click", openPopup)
+  function openPopup() {
+    popup.classList.add("open-popup")
+  }
+
+  // Close Pop up
+  let close = document.getElementById("close-popup")
+  close.addEventListener("click", closePopup)
+  function closePopup() {
+    popup.classList.remove("open-popup")
+  }
